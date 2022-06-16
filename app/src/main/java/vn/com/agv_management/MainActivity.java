777bbd1ec03b70e6b8347ec43cb26c154e7b108c
Nguyen Dinh Sang <sangckm93@gmail.com>
2022-06-16 20:03:27 +0700

    Initial commit

---
+++ app/src/main/java/vn/com/agv_management/MainActivity.java
@@ -0,0 +1,25 @@
+package vn.com.agv_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
